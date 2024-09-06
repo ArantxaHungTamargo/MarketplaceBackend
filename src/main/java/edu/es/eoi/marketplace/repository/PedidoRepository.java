ad9a536912904abba970d78d1906696962c54abb
Manuel Seva <manuelsevatrompet@gmail.com>
2024-09-06 13:34:14 +0200

    My first Commit

---
+++ src/main/java/edu/es/eoi/marketplace/repository/PedidoRepository.java
@@ -0,0 +1,7 @@
+package edu.es.eoi.marketplace.repository;
+
+import edu.es.eoi.marketplace.entity.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+}
